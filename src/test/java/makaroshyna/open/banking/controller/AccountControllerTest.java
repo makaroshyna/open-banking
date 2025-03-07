2d9ffe62e2103a1ab374c1105f023864f85fe75a
makaroshyna <katya23225@gmail.com>
2025-03-07 23:13:01 +0200

    Add integration test for AccountController

---
+++ src/test/java/makaroshyna/open/banking/controller/AccountControllerTest.java
@@ -0,0 +1,35 @@
+package makaroshyna.open.banking.controller;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import makaroshyna.open.banking.service.AccountService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(AccountController.class)
+public class AccountControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockitoBean
+    private AccountService accountService;
+
+    @Test
+    void getAccountBalance_ShouldReturnBalance() throws Exception {
+        when(accountService.getAccountBalance("DE89370400440532013000"))
+                .thenReturn(new BigDecimal("5000.00"));
+
+        mockMvc.perform(get("/api/accounts/DE89370400440532013000/balance"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("5000.00"));    }
+}
