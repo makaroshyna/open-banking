690db19db4f49fc24bfb64562cf195b19768c490
makaroshyna <katya23225@gmail.com>
2025-03-07 20:24:35 +0200

    Modify AccountService to use Mock API

--- src/main/java/makaroshyna/open/banking/service/AccountService.java
+++ src/main/java/makaroshyna/open/banking/service/AccountService.java
@@ -2,32 +2,22 @@ package makaroshyna.open.banking.service;
 
 import java.math.BigDecimal;
 import java.util.List;
-import java.util.Optional;
-import makaroshyna.open.banking.model.Account;
 import makaroshyna.open.banking.model.Transaction;
-import makaroshyna.open.banking.repository.AccountRepository;
-import makaroshyna.open.banking.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AccountService {
-    private final AccountRepository accountRepository;
-    private final TransactionRepository transactionRepository;
+    private final BankingClientService bankingClientService;
 
-    public AccountService(AccountRepository accountRepository, TransactionRepository transactionRepository) {
-        this.accountRepository = accountRepository;
-        this.transactionRepository = transactionRepository;
+    public AccountService(BankingClientService bankingClientService) {
+        this.bankingClientService = bankingClientService;
     }
 
-    @Transactional(readOnly = true)
     public BigDecimal getAccountBalance(String iban) {
-        Optional<Account> account = accountRepository.findById(iban);
-        return account.map(Account::getBalance).orElseThrow(() -> new RuntimeException("Account not found"));
+        return bankingClientService.getExternalAccountBalance(iban);
     }
 
-    @Transactional(readOnly = true)
     public List<Transaction> getRecentTransactions(String iban) {
-        return transactionRepository.findTop10ByAccountIbanOrderByTimestampDesc(iban);
+        return bankingClientService.getExternalTransactions(iban);
     }
 }
