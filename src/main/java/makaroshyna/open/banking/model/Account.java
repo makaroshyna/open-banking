54d3164b29be8ac2e3c797d1bf85996feb397bc5
makaroshyna <katya23225@gmail.com>
2025-03-07 19:37:59 +0200

    Add account model

---
+++ src/main/java/makaroshyna/open/banking/model/Account.java
@@ -0,0 +1,45 @@
+package makaroshyna.open.banking.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "account")
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+    @Id
+    private String iban;
+    private String ownerName;
+    private BigDecimal balance;
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
