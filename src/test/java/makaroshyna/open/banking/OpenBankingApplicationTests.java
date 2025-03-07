b8ebc3ded2d47a5159f04e1872ca2bd7297dd876
makaroshyna <katya23225@gmail.com>
2025-03-07 19:37:18 +0200

    Init commit

---
+++ src/test/java/makaroshyna/open/banking/OpenBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package makaroshyna.open.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
